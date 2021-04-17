007fa7b446d9777593e4be77033dd1e85dd3ed8b
Alanzinho61 <mucahit.yucel61@gmail.com>
2021-04-17 05:31:38 +0300

    xml secildi

--- Vize/Vize/Program.cs
+++ Vize/Vize/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Xml;
 
 namespace Vize
 {
     class Program
     {
+        //public static XElement root;
         static void Main(string[] args)
         {
+            string bugun = "https://www.haberturk.com/rss";
+            var xmldosya = new XmlDocument();
+            xmldosya.Load(bugun);
+            DateTime tarih = Convert.ToDateTime(xmldosya.SelectSingleNode("//pubDate").Attributes["pubDate"].Value);
+            string haber = xmldosya.SelectSingleNode("pubDate/item[@kod='title']").InnerXml;
+            //Console.WriteLine(tarih);
+            Console.ReadKey();
         }
     }
 }
