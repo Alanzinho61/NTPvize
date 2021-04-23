0b0abadeaf947b20a8e83409e23a92bb3511e046
Alanzinho61 <mucahit.yucel61@gmail.com>
2021-04-24 01:07:24 +0300

    Tamamlandi

--- Vize/Vize/Program.cs
+++ Vize/Vize/Program.cs
@@ -3,23 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
+using System.Xml.Linq;
 using System.Xml;
 
 namespace Vize
 {
     class Program
     {
-        //public static XElement root;
+        public static XElement root;
+        public static XMLGET xml = new XMLGET();
         static void Main(string[] args)
         {
-            string bugun = "https://www.haberturk.com/rss";
-            var xmldosya = new XmlDocument();
-            xmldosya.Load(bugun);
-            DateTime tarih = Convert.ToDateTime(xmldosya.SelectSingleNode("//pubDate").Attributes["pubDate"].Value);
-            string haber = xmldosya.SelectSingleNode("pubDate/item[@kod='title']").InnerXml;
-            //Console.WriteLine(tarih);
+            XMLGET.ac();
+            XMLGET.yazdir();
+            XMLGET.kaydet();   
             Console.ReadKey();
+            
         }
+        public class XMLGET
+        {
+            public static void ac()
+            {
+                XmlDocument yenixml = new XmlDocument();
+                yenixml.Load("Haberturk.Xml");
+            }
+            public static void yazdir()
+            {
+                XmlDocument cekilecekxml = new XmlDocument();
+                cekilecekxml.Load("https://www.haberturk.com/rss");
+
+                XElement root = XElement.Load("Haberturk.Xml");
+
+                root.RemoveAll();
+                foreach (XmlNode item in cekilecekxml.SelectNodes("rss/channel/item"))
+                {
+                    Console.WriteLine(item["title"].InnerText);
+
+                    XElement medya = new XElement("medya");
+                    XElement baslik = new XElement("baslik");
+
+                    baslik.Value = item["title"].InnerText;
+
+                    medya.Add(baslik);
+                    root.Add(medya);
+
+                }
+             
+            }
+            public static void kaydet()
+            {
+                XElement root = XElement.Load("Haberturk.Xml");
+                root.Save("Haberturk.Xml");
+            }
+        }
+
+        
     }
 }
